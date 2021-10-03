3a91cffd980ec5134c9fc88104da5f21da584a38
Mariano Renna <mariano1177@hotmail.com>
2021-10-03 17:14:02 -0300

    act1

---
+++ Guia 1/Ejercicio1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 200, n2 = 56;
+            Console.WriteLine("la suma de {0} y {1} es: {2}", n1, n2, n1 + n2);
+        }
+    }
+}
