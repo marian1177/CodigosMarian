0c658f73cb3b902d456c4d2877c4748be27f16ab
Mariano Renna <mariano1177@hotmail.com>
2021-10-03 17:20:55 -0300

    act2

---
+++ Guia 1/ejercicio3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crea un programa que diga el resultado de sumar 200+36
+            int n1 = 200, n2 = 36, Resultado;
+            Resultado = n1 + n2;
+            Console.WriteLine("el resultado de la suma de 200 y 36 es: {0} ",Resultado);
+        }
+    }
+}
